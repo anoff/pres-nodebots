cbc37831d7801210a925d6f77141fb25352e1f04
anoff <offenhaeuser@gmail.com>
2017-03-07 17:44:48 +0100

    :star2: feat: moar content

--- presentation/index.js
+++ presentation/index.js
@@ -2,16 +2,13 @@ import React from "react";
 
 // Import Spectacle Core tags
 import {
-  BlockQuote,
-  Cite,
   Deck,
   Heading,
   Image,
+  Link,
   ListItem,
   List,
   Markdown,
-  MarkdownSlides,
-  Quote,
   Slide,
   Text
 } from "spectacle";
@@ -27,10 +24,10 @@ require("normalize.css");
 require("spectacle/lib/themes/default/index.css");
 
 const theme = createTheme({
-  primary: "#d0d0ff",
+  primary: "#efefef",
   secondary: "#1F2022",
   tertiary: "#03A9FC",
-  quartenary: "#CECECE"
+  quartenary: "#a110ff"
 }, {
   primary: "Montserrat",
   secondary: "Helvetica"
@@ -41,7 +38,11 @@ const images = {
   me: require("../assets/iceicesquare.jpg"),
   digime: require("../assets/leg.jpg"),
   gh: require("../assets/gh.svg"),
-  twitter: require("../assets/twitter.png")
+  twitter: require("../assets/twitter.png"),
+  firmata1: require("../assets/firmata.png"),
+  firmata2: require("../assets/firmata-control.png"),
+  firmata3: require("../assets/firmata-all.png"),
+  remote: require("../assets/remotecode.png")
 };
 
 preloader(images);
@@ -67,30 +68,64 @@ export default class Presentation extends React.Component {
           <Text margin="0 auto"><Image src={images.twitter} height="40px" margin="0 20px auto"/>@an0xFF</Text>
           <Text margin="0 auto"><Image src={images.gh} height="40px" margin="0 20px auto"/>github.com/anoff</Text>
         </Slide>
-        {
-          MarkdownSlides`
-# johnny-five
-* firmata
-* realtimeliness
-* ports
-* sensor limits (pulsed signals)
----
-# differences firmata j5
-* no repl
-* supporting library (communication) of j5
-* auto-discovery of boards
-* device configuration has to be done manually
----
-# raspi-io
-* limited io http://johnny-five.io/platform-support/#raspberry-pi-3-model-b
-* needs expander
-
-          `
-        }
         <Slide>
-          <Heading size={3}>Attribution</Heading>
-          <Text>find the source for this talk on github @ pres-nodebots-2017-03-09</Text>
-          <Text>images used are listed in the readme </Text>
+          <Heading>johnny-five</Heading>
+          <Markdown>{`
+* javascript robotics framework
+* abstracts low level hardware
+* [huge hardware platform support](http://johnny-five.io/platform-support/)
+* uses firmata under the hood
+            `}</Markdown>
+        </Slide>
+        <Slide>
+          <Heading>j5 & firmata</Heading>
+          <Image src={images.firmata1} />
+        </Slide>
+        <Slide>
+          <Heading>j5 & firmata</Heading>
+          <Image src={images.firmata3} />
+        </Slide>
+        <Slide>
+          <Heading>Demo time ⚒</Heading>
+          <Heading size="4">#1 Servo / Sonar on Arduino</Heading>
+        </Slide>
+        <Slide>
+          <Heading fit>firmata (protocol) limitations</Heading>
+          <Markdown>{`
+* limited support for arduino libs
+* no realtime signal analysis 🕵️
+            `}</Markdown>
+        </Slide>
+        <Slide>
+          <Heading fit>johnny-five vs firmata.js</Heading>
+            <Markdown>{`
+* j5 uses firmata.js for communication
+* incl. REPL (Read–Eval–Print Loop) ⌨️
+* auto-discovery of attached boards 👀
+* js classes w/ setup routines 📦
+            `}</Markdown>
+        </Slide>
+        <Slide>
+          <Heading>Demo time ⚒</Heading>
+          <Heading size="4" fit>#2 LEDs on raspberry pi</Heading>
+        </Slide>
+        <Slide>
+          <Heading fit>limitations of raspi-io</Heading>
+            <Markdown>{`
+* limited [raspi hardware IO](http://johnny-five.io/platform-support/#raspberry-pi-3-model-b)
+* no realtime possible
+            `}</Markdown>
+        </Slide>
+        <Slide>
+          <Heading>Demo time ⚒</Heading>
+          <Image src={images.remote} />
+        </Slide>
+        <Slide>
+          <Heading size={2}>Attribution</Heading>
+          <List>
+            <ListItem>find the source for this talk on <Link href="https://github.com/anoff/pres-nodebots-2017-03-09"> github</Link></ListItem>
+            <ListItem>images used are listed in the <Link href="https://github.com/anoff/pres-nodebots-2017-03-09/blob/master/readme.md">readme</Link></ListItem>
+          </List>
         </Slide>
       </Deck>
     );
---
+++ readme.md
@@ -0,0 +1,22 @@
+# nodebots
+
+> published version at **[http://nodebots.surge.sh/](http://nodebots.surge.sh/)**
+
+My presentation held during [@nodemeetup](https://www.meetup.com/nodemeetup/) in march 2017 on the nodebots ecosystem
+
+based on [Spectacle Boilerplate](https://github.com/FormidableLabs/spectacle-boilerplate/) ([API](https://github.com/FormidableLabs/spectacle/blob/master/README.markdown))
+
+# further links
+
+* [Rick Waldron explaining firmata](https://bocoup.com/blog/intel-galileo-javascript-nodejs/)
+
+# image attributions
+
+* [overview.png](./assets/overview.png) is composed of the following sourced and shared under CC-BY-SA-4.0
+  * LED: https://commons.wikimedia.org/wiki/File:Diodos_LED.svg
+  * robot: https://en.wikipedia.org/wiki/File:Cartoon_Robot.svg
+  * johnny-five/node/nodebots logo from respective webpage
+  * robot car: own image
+* j5-firmata.png: https://bocoup.com/blog/intel-galileo-javascript-nodejs/
+* [firmata(-control/-all).png, overview.png](./assets/firmata.png) own licensed under CC-BY-SA, composed of following:
+  * computer: https://commons.wikimedia.org/wiki/File:Devicetemplates_laptop-02.png
